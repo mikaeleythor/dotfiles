8e1f5798e1771c37bbd4cc83779a2f097fafb0df
Eythor Mikael <eythor108@gmail.com>
2022-05-06 13:34:43 +0200

    fix folder structure

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,6 +2,6 @@
 
 killall -q polybar
 
-polybar -c $HOME/.config/polybar/config-new main 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -c $HOME/.config/polybar/config main 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched.."
